731f066acd4214be80a8f94cf12433896f34fd94
Brijesh Yadav <brijeshyadavmu@gmail.com>
2024-02-27 20:08:08 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose");
+const initData=require("./data.js");
+const Listing=require("../models/listing.js");
+
+const MONGO_URL="mongodb://127.0.0.1:27017/StayHub";
+
+main()
+.then(()=>{
+    console.log("connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+})
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB=async ()=>{
+    await Listing.deleteMany({});
+   initData.data= initData.data.map((obj)=>({...obj,owner:"65d43f0041ced452284c3c93"}));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
+
+initDB();
